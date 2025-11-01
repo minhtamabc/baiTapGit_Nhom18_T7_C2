9dbba0b278b82095221fd7ae9b17088f34552113
Nguyen Minh Tam <nminhtam425@gmail.com>
2025-11-01 10:40:16 +0700

    hoan thanh ham main

--- Source.cpp
+++ Source.cpp
@@ -12,13 +12,17 @@ int tich(int a, int b){
 }
 float thuong(int a, int b){
 	if(b == 0)
-		return 0;
+		return -1;
 	return a/b;
 }
 int main()
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	int a = 10, b = 2;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT\n";
+	cout<<"Tong ="<<tong(a,b);
+	cout<<"Hieu ="<<hieu(a,b);
+	cout<<"Tich ="<<tich(a,b);
+	cout<<"Thuong ="<<thuong(a,b);
 	system("pause");
 	return 0;
 }
