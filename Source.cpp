74d50d07c3476bcb0b65321971d4f637d12018f2
Nguyen Minh Tam <nminhtam425@gmail.com>
2025-11-01 10:25:35 +0700

    khoi tao du an va hoan thanh cac ham

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int tong(int a, int b){
+	return a+B;
+}
+int hieu(int a, int b){
+	return a-b;
+}
+int tich(int a, int b){
+	return a*b;
+}
+float thuong(int a, int b){
+	if(b == 0)
+		return 0;
+	return a/b;
+}
+int main()
+{
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	cout<<"Tong =";
+	system("pause");
+	return 0;
+}
